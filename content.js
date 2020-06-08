caa676d36036268b5b6cbdb0284cbf785a8eb4cb
foo <foo@bar.de>
2020-06-08 20:53:23 +0200

    fix: show header nav on all pages

--- content.js
+++ content.js
@@ -4,16 +4,12 @@ import buildActionButton from './model/build-action-button'
 import isApplicable from './model/is-applicable'
 
 const run = async () => {
-  if (!isApplicable) {
-    return undefined
-  }
-
   const savedFilters =
     (browser.storage.sync.get('filters') |> await |> property('filters')) || []
 
   return Promise.all([
     buildHeaderNav({ savedFilters }),
-    buildActionButton({ savedFilters }),
+    ...(isApplicable ? [buildActionButton({ savedFilters })] : []),
   ])
 }
 
